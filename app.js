920e3029035a338c3c646eb695a4907d47f980a4
Edmo <edmogc@gmail.com>
2024-09-06 14:13:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function pesquisar() {
-    let section = document.getElementById("resultado-pesquisa");
+    let section = document.getElementById
+    ("resultado-pesquisa");
 // busque o elemento pelo ID dele.
 
 console.log(section);
@@ -7,9 +8,28 @@ console.log(section);
 // Limpa o conteúdo da seção antes de adicionar novos dados
 section.innerHTML = '';
 
-let resultado = ""
+let campoPesquisa = document.getElementById
+("campo-pesquisa").value
+
+if(!campoPesquisa){
+    section.innerHTML = "<p>Nada foi encontrado</p>"
+    return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+console.log(campoPesquisa);
+
+let resultado = "";
+let titulo = "";
+let descricao = "";
 
 for (let dado of dados){
+
+    titulo = dado.titulo.toLocaleLowerCase()
+    descricao = dado.descricao.toLocaleLowerCase()
+    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+        
     resultado += `
     <div class="item-resultado">
             <h2>
@@ -19,6 +39,11 @@ for (let dado of dados){
             <a href=${dado.link} target="_blank">Mais Informações</a>
         </div> 
 `;
+    }
+}
+
+if(!resultado) {
+    resultado = "<p>Nada foi encontrado</p>"
 }
 
 section.innerHTML = resultado
