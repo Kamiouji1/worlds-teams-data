8534ae0f7d7f1eebf26911fe8c032d14ea6f5d9a
Edmo <edmogc@gmail.com>
2024-09-09 12:06:07 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,15 +1,64 @@
 let dados = [
-    // Times da LCK
-    {titulo : "SK Telecom T1 (T1)", descricao : "A T1, anteriormente conhecida como SK Telecom T1, é uma das organizações de eSports mais prestigiosas e bem-sucedidas do mundo, especialmente no cenário competitivo de League of Legends.Com uma história rica em títulos internacionais, a T1 se tornou sinônimo de excelência e domínio no jogo. A equipe sul-coreana conquistou o coração dos fãs ao redor do globo com seu estilo de jogo agressivo e estratégico, formando diversas dinastias e inspirando novas gerações de jogadores profissionais. A T1 é mais do que apenas uma equipe; é uma marca que representa a paixão, a dedicação e a busca incansável pela perfeição no mundo dos eSports.",link : "https://www.t1.gg/"},
-    {titulo : "Gen.G Esports (GenG)", descricao : "A Gen.G é outro gigante do League of Legends, conhecido por sua consistência e jogadas estratégicas. Com uma torcida apaixonada, a Gen.G busca sempre alcançar o topo do cenário competitivo.", link : "https://www.gen.gg/"},
-    {titulo : "Dplus KIA (DK)", descricao : "A DK, anteriormente conhecida como Dplus KIA, é uma das equipes mais consistentes do League of Legends coreano, com um estilo de jogo agressivo e focado em objetivos. Com um elenco talentoso e uma torcida apaixonada, a DK busca sempre dominar o cenário competitivo.", link : "https://dpluskia.com/"},
-    {titulo : "Hanwha Life Esports (HLE)", descricao : "A Hanwha Life Esports, abreviada como HLE, é uma equipe em ascensão no cenário competitivo. Com um elenco talentoso e uma organização ambiciosa, a HLE busca se consolidar entre as principais equipes da Coreia do Sul.",link : "https://www.hanwhalifeesports.com/"},
+   // LCK (Coreia do Sul)
 
+    {
+      titulo: "SK Telecom T1 (T1)",
+      descricao: "A T1, anteriormente conhecida como SK Telecom T1, é uma das organizações de eSports mais prestigiosas e bem-sucedidas do mundo, especialmente no cenário competitivo de League of Legends.Com uma história rica em títulos internacionais, a T1 se tornou sinônimo de excelência e domínio no jogo. A equipe sul-coreana conquistou o coração dos fãs ao redor do globo com seu estilo de jogo agressivo e estratégico, formando diversas dinastias e inspirando novas gerações de jogadores profissionais. A T1 é mais do que apenas uma equipe; é uma marca que representa a paixão, a dedicação e a busca incansável pela perfeição no mundo dos eSports.",
+      link: "https://www.t1.gg/",
+      elenco: [
+        "Zeus",
+        "Oner",
+        "Faker",
+        "Gumayusi",
+        "Keria"
+      ],
+      logo: "https://www.t1.gg/logo.png"
+    },
+    {
+      titulo: "Gen.G Esports (GenG)",
+      descricao: "A Gen.G é outro gigante do League of Legends, conhecido por sua consistência e jogadas estratégicas. Com uma torcida apaixonada, a Gen.G busca sempre alcançar o topo do cenário competitivo.",
+      link: "https://www.gen.gg/",
+      elenco: [
+        "Kiin",
+        "Canyon",
+        "Chovy",
+        "Peyz",
+        "Lehends"
+      ],
+      logo: "https://www.gen.gg/logo.png"
+    },
+    {
+      titulo: "Dplus KIA (DK)",
+      descricao: "A DK, anteriormente conhecida como Dplus KIA, é uma das equipes mais consistentes do League of Legends coreano, com um estilo de jogo agressivo e focado em objetivos. Com um elenco talentoso e uma torcida apaixonada, a DK busca sempre dominar o cenário competitivo.",
+      link: "https://dpluskia.com/",
+      elenco: [
+        "Kingen",
+        "Lucid",
+        "ShowMaker",
+        "Aiming",
+        "Moham"
+      ],
+      logo: "https://dpluskia.com/logo.png"
+    },
+    {
+      titulo: "Hanwha Life Esports (HLE)",
+      descricao: "A Hanwha Life Esports, abreviada como HLE, é uma equipe em ascensão no cenário competitivo. Com um elenco talentoso e uma organização ambiciosa, a HLE busca se consolidar entre as principais equipes da Coreia do Sul.",
+      link: "https://www.hanwhalifeesports.com/",
+      elenco: [
+        "Doran",
+        "Peanut",
+        "Zeka",
+        "Viper",
+        "Delight"
+      ],
+      logo: "https://www.hanwhalifeesports.com/logo.png"
+    },
+    
     // Times da LPL
     {titulo: "Bilibili Gaming (BLG)",descricao: "A Bilibili Gaming é uma das equipes mais novas a se destacar na LPL. Com um elenco talentoso e uma base de fãs apaixonada, a BLG busca constantemente se consolidar entre as principais equipes da China.", link: "https://www.bilibili.com/esports"},
     {titulo: "Top Esports (TES)", descricao: "A Top Esports é uma das equipes mais tradicionais e bem-sucedidas da LPL. Com uma história de conquistas e um estilo de jogo agressivo, a TES sempre é uma forte candidata aos títulos.", link: "https://www.topsports-gaming.com/"},
     {titulo: "LNG Esports (LNG)", descricao: "A LNG Esports é uma equipe em constante evolução, buscando se tornar uma das forças dominantes da LPL. Com um elenco jovem e talentoso, a LNG promete ser uma das equipes a ser observada nos próximos anos.", link: "https://lngesports.com/"},
-    {titulo: "Weibo Gaming (Weibo)", descricao: "A Weibo Gaming é uma equipe com uma base de fãs apaixonada e um elenco que busca constantemente se aprimorar. Com uma história recente de bons resultados, a Weibo está pronta para desafiar os principais times da LPL.", link: "https://weibo.com/u/6159715078"}, // Adapte o link se necessário
+    {titulo: "Weibo Gaming (Weibo)", descricao: "A Weibo Gaming é uma equipe com uma base de fãs apaixonada e um elenco que busca constantemente se aprimorar. Com uma história recente de bons resultados, a Weibo está pronta para desafiar os principais times da LPL.", link: "https://weibo.com/u/6159715078"},
    
     // LEC (Europa)
     {titulo: "G2 Esports", descricao: "Uma das equipes mais populares e bem-sucedidas da Europa, conhecida por seu estilo de jogo agressivo e jogadas inovadoras.", link: "https://www.g2esports.com/"},
@@ -20,6 +69,29 @@ let dados = [
     {titulo: "Cloud9", descricao: "Uma das equipes mais antigas e tradicionais da LCS, com uma grande base de fãs.", link: "https://www.cloud9.gg/"},
 
      // CBLOL (Brasil)
-     {titulo: "paiN Gaming",descricao: "Uma das equipes mais tradicionais do Brasil, conhecida por sua paixão e dedicação ao cenário competitivo de League of Legends.", link: "https://www.pain.gg/"}
+     {titulo: "paiN Gaming",descricao: "Uma das equipes mais tradicionais do Brasil, conhecida por sua paixão e dedicação ao cenário competitivo de League of Legends.", link: "https://www.pain.gg/"},
+
+     // PCS (Ásia-Pacífico)
+     {
+        titulo: "PSG Talon",
+        descricao: "Uma equipe taiwanesa conhecida por seu estilo de jogo agressivo e jogadores talentosos, apoiada pela marca esportiva PSG.",
+        link: "https://www.ps-gaming.com/"
+      },
+      {
+        titulo: "Fukuoka SoftBank HAWKS gaming",
+        descricao: "Uma equipe japonesa conhecida por sua habilidade em jogos de equipe e estratégias inovadoras.",
+        link: "https://www.hawks-gaming.jp/"
+      },
+     // VCS (Sudeste Asiático)
+    {
+      titulo: "VCS Vikings Esports",
+      descricao: "Uma equipe vietnamita conhecida por sua habilidade em jogos de equipe e estratégias inovadoras.",
+      link: "https://www.vcs-vikings.com/"
+    },
+    {
+      titulo: "GAM Esports",
+      descricao: "Uma equipe vietnamita conhecida por seu estilo de jogo agressivo e jogadores talentosos, com uma longa história de sucesso em competições regionais.",
+      link: "https://www.gam-esports.vn/"
+    }
       
 ];
