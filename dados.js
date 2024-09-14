56d2e4a9f4ac7cf62e6b3908a3fa4e6b3410dd26
Edmo <edmogc@gmail.com>
2024-09-14 18:03:36 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -4,7 +4,7 @@ let dados = [
     "regiao": "lck",
     "times": [
       {
-        "titulo": "SK Telecom T1",
+        "titulo": "SK Telecom T1 #4",
         "descricao": "A T1, anteriormente conhecida como SK Telecom T1, é uma das organizações de eSports mais prestigiosas e bem-sucedidas do mundo, especialmente no cenário competitivo de League of Legends.Com uma história rica em títulos internacionais, a T1 se tornou sinônimo de excelência e domínio no jogo. A equipe sul-coreana conquistou o coração dos fãs ao redor do globo com seu estilo de jogo agressivo e estratégico, formando diversas dinastias e inspirando novas gerações de jogadores profissionais. A T1 é mais do que apenas uma equipe; é uma marca que representa a paixão, a dedicação e a busca incansável pela perfeição no mundo dos eSports.",
         "link": "https://www.t1.gg/",
         "elenco": [
@@ -17,7 +17,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/commons/3/3a/T1_esports_logo.svg"
       },
       {
-        "titulo": "Gen.G Esports",
+        "titulo": "Gen.G Esports #2",
         "descricao": "A Gen.G é outro gigante do League of Legends, conhecido por sua consistência e jogadas estratégicas. Com uma torcida apaixonada, a Gen.G busca sempre alcançar o topo do cenário competitivo.",
         "link": "https://www.gen.gg/",
         "elenco": [
@@ -30,7 +30,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2d/Gen.G_logo.png"
       },
       {
-        "titulo": "Dplus KIA",
+        "titulo": "Dplus KIA #3",
         "descricao": "A DK, anteriormente conhecida como Dplus KIA, é uma das equipes mais consistentes do League of Legends coreano, com um estilo de jogo agressivo e focado em objetivos. Com um elenco talentoso e uma torcida apaixonada, a DK busca sempre dominar o cenário competitivo.",
         "link": "https://dpluskia.com/",
         "elenco": [
@@ -43,7 +43,8 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/commons/2/20/DPlus_KIA_Logo.svg"
       },
       {
-        "titulo": "Hanwha Life",
+        "titulo": "Hanwha Life Esports #1",
+
         "descricao": "A Hanwha Life Esports, abreviada como HLE, é uma equipe em ascensão no cenário competitivo. Com um elenco talentoso e uma organização ambiciosa, a HLE busca se consolidar entre as principais equipes da Coreia do Sul.",
         "link": "https://www.hanwhalifeesports.com/",
         "elenco": [
@@ -62,7 +63,7 @@ let dados = [
     "regiao": "lpl",
     "times": [
       {
-        "titulo": "Bilibili Gaming",
+        "titulo": "Bilibili Gaming #1",
         "descricao": "A Bilibili Gaming é uma das equipes mais novas a se destacar na LPL. Com um elenco talentoso e uma base de fãs apaixonada, a BLG busca constantemente se consolidar entre as principais equipes da China.",
         "link": "https://www.bilibili.com/esports",
         "elenco": [
@@ -75,7 +76,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/en/6/66/Bilibili_Gaming_logo_%282021%29.png"
       },
       {
-        "titulo": "Top Esports",
+        "titulo": "Top Esports #2",
         "descricao": "A Top Esports é uma das equipes mais tradicionais e bem-sucedidas da LPL. Com uma história de conquistas e um estilo de jogo agressivo, a TES sempre é uma forte candidata aos títulos.",
         "link": "https://www.topsports-gaming.com/",
         "elenco": [
@@ -88,7 +89,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/en/a/ac/Top_Esports_logo.png"
       },
       {
-        "titulo": "LNG Esports",
+        "titulo": "LNG Esports #3",
         "descricao": "A LNG Esports é uma equipe em constante evolução, buscando se tornar uma das forças dominantes da LPL. Com um elenco jovem e talentoso, a LNG promete ser uma das equipes a ser observada nos próximos anos.",
         "link": "https://lngesports.com/",
         "elenco": [
@@ -101,7 +102,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/pt/b/b4/League_of_legends_pro_league_logo.svg"
       },
       {
-        "titulo": "Weibo Gaming",
+        "titulo": "Weibo Gaming #4",
         "descricao": "A Weibo Gaming é uma equipe com uma base de fãs apaixonada e um elenco que busca constantemente se aprimorar. Com uma história recente de bons resultados, a Weibo está pronta para desafiar os principais times da LPL.",
         "link": "https://weibo.com/u/6159715078",
         "elenco": [
@@ -120,7 +121,7 @@ let dados = [
     "regiao": "lec",
     "times": [
       {
-        "titulo": "G2 Esports",
+        "titulo": "G2 Esports #1",
         "descricao": "Uma das equipes mais populares e bem-sucedidas da Europa, conhecida por seu estilo de jogo agressivo e jogadas inovadoras. A G2 Esports teve um ano impressionante em 2024. Eles dominaram o League of Legends EMEA Championship, vencendo as finais da temporada com uma vitória de 3-1 sobre a Fnatic. ",
         "link": "https://www.g2esports.com/",
         "elenco": [
@@ -133,7 +134,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/pt/2/23/G2_Esports_logo.png"
       },
       {
-        "titulo": "Fnatic",
+        "titulo": "Fnatic #2",
         "descricao": "Uma das equipes mais tradicionais e bem-sucedidas da Europa, conhecida por sua consistência e habilidade em competições internacionais. Fnatic teve um ano movimentado em 2024. No League of Legends, eles se classificaram para o Mundial de LoL após ficarem entre os três primeiros na LEC Season Finals.",
         "link": "https://fnatic.com/",
         "elenco": [
@@ -146,7 +147,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/pt/f/f0/Fnatic_logo.png"
       },
       {
-        "titulo": "MAD Lions",
+        "titulo": "MAD Lions #3",
         "descricao": "Uma equipe emergente na Europa, reconhecida por seu estilo de jogo estratégico e jogadores talentosos. MAD Lions também teve um ano notável. Eles se classificaram para o Mundial de LoL, destacando-se na LEC1. Além disso, a equipe manteve uma forte presença no cenário europeu, com Elyoya continuando como um dos jogadores-chave.",
         "link": "https://madlions.com/",
         "elenco": [
@@ -165,7 +166,7 @@ let dados = [
     "regiao": "lcs",
     "times": [
       {
-        "titulo": "FlyQuest",
+        "titulo": "FlyQuest #1",
         "descricao": "A FlyQuest é uma equipe da LCS conhecida por seus jogadores talentosos e seu estilo de jogo agressivo. A equipe busca constantemente se aprimorar e se consolidar entre os principais times da região.",
         "link": "https://flyquest.com/",
         "elenco": [
@@ -178,7 +179,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/commons/c/c1/Flyquest_logo_2021.svg" // URL da logo da FlyQuest
       },
       {
-        "titulo": "Team Liquid",
+        "titulo": "Team Liquid #2",
         "descricao": "O Team Liquid é um dos times mais tradicionais e bem-sucedidos da LCS. Conhecida por sua consistência e jogadores experientes, a equipe sempre busca desafiar o topo da região.",
         "link": "https://www.teamliquid.com/",
         "elenco": [
@@ -191,7 +192,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/pt/4/4b/Teamliquid_logo_blue.png" 
       },
       {
-        "titulo": "100 Thieves",
+        "titulo": "100 Thieves #3",
         "descricao": "Os 100 Thieves são uma organização de esports relativamente nova na LCS, mas que rapidamente conquistou uma grande base de fãs. A equipe busca se consolidar como um dos principais times da região, com um estilo de jogo inovador e agressivo.",
         "link": "https://100thieves.com/",
         "elenco": [
@@ -210,7 +211,7 @@ let dados = [
     "regiao": "cblol",
     "times": [
       {
-        "titulo": "paiN Gaming",
+        "titulo": "paiN Gaming #1",
         "descricao": "A paiN Gaming teve um ano incrível no CBLOL 2024. Eles conquistaram o título do 2º Split ao derrotar a Vivo Keyd Stars por 3 a 1 na grande final12. Com essa vitória, a paiN se tornou tetracampeã do CBLOL e garantiu sua vaga no Mundial de League of Legends (Worlds 2024), onde enfrentarão a PSG Talon na primeira rodada.",
         "link": "https://www.soupain.com.br/",
         "elenco": [
@@ -229,7 +230,7 @@ let dados = [
   "regiao": "pcs",
   "times": [
     {
-      "titulo": "PSG Talon",
+      "titulo": "PSG Talon #1",
       "descricao": "PSG Talon teve um ano sólido em 2024. Eles se destacaram na PCS, garantindo uma vaga no Mundial de League of Legends após uma performance dominante na temporada regular e nos playoffs1. A equipe continua a ser uma das forças mais consistentes na região, com jogadores como Unified e Kaiwing desempenhando papéis cruciais.",
       "link": "https://www.ps-gaming.com/",
       "elenco": [
@@ -242,7 +243,7 @@ let dados = [
       "logo": "https://am-a.akamaihd.net/image?resize=200:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1644501567410_PSG_3.png"
     },
     {
-      "titulo": "Fukuoka SoftBank HAWKS gaming",
+      "titulo": "Fukuoka SoftBank HAWKS gaming #2",
       "descricao": "Também teve um ano notável. Eles venceram o LJL (League of Legends Japan League) Summer Split de 2024, garantindo sua vaga no Mundial de LoL2. A equipe, liderada por jogadores como Evi e DasheR, mostrou um desempenho impressionante ao longo do ano.",
       "link": "https://www.hawks-gaming.jp/",
       "elenco": [
@@ -261,7 +262,7 @@ let dados = [
     "regiao": "vcs",
     "times": [
       {
-        "titulo": "VCS Vikings Esports",
+        "titulo": "VCS Vikings Esports #2",
         "descricao": "Uma equipe vietnamita conhecida por sua habilidade em jogos de equipe e estratégias inovadoras.",
         "link": "https://www.vcs-vikings.com/",
         "elenco": [
@@ -274,7 +275,7 @@ let dados = [
         "logo": "https://upload.wikimedia.org/wikipedia/en/5/52/Vikings_Esports_logo.png"
       },
       {
-        "titulo": "GAM Esports",
+        "titulo": "GAM Esports #1",
         "descricao": "Uma equipe vietnamita conhecida por seu estilo de jogo agressivo e jogadores talentosos, com uma longa história de sucesso em competições regionais.",
         "link": "https://www.gam-esports.vn/",
         "elenco": [
@@ -294,7 +295,7 @@ let dados = [
     "regiao": "lla",
     "times": [
       {
-        "titulo": "Movistar R7",
+        "titulo": "Movistar R7 #1",
         "descricao": "A Movistar R7 é uma das equipes mais tradicionais e bem-sucedidas da LLA, conhecida por sua história de conquistas e por seus jogadores talentosos. A equipe busca se consolidar como um dos principais times da região, com um estilo de jogo agressivo e focado em objetivos.",
         "link": "https://www.movistar.com.ar/esports/",
         "elenco": [
